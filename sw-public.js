7473f9572c08c48a30cdd8b95bb88a7269a34acd
Cobras Paris <cobras-paris@users.noreply.github.com>
2026-08-27 11:25:23 +0200

    Forcer la mise a jour du cache staff

--- sw-public.js
+++ sw-public.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cobras-public-v1";
+const CACHE_NAME = "cobras-public-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./calendrier.html", "./calendrier.css", "./calendrier.js", "./inscription.html", "./cobras-logo.png", "./manifest-public.json"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
