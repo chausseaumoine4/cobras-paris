c34035bae40c70217142f6a80f16b613ef3a0c3b
Cobras Paris <cobras-paris@users.noreply.github.com>
2026-08-27 15:14:02 +0200

    Corriger le lancement de l application staff

--- sw-staff.js
+++ sw-staff.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cobras-staff-v2";
+const CACHE_NAME = "cobras-staff-v3";
 const ASSETS = ["./", "./staff.html", "./staff.js", "./styles.css", "./cobras-logo.png", "./manifest-staff.json"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
