32a043728bf9266a70a3de646431ee0278388922
Cobras Paris <cobras-paris@users.noreply.github.com>
2026-08-27 11:00:06 +0200

    Ajouter les deux applications web installables

---
+++ sw-public.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = "cobras-public-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./calendrier.html", "./calendrier.css", "./calendrier.js", "./inscription.html", "./cobras-logo.png", "./manifest-public.json"];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
+self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
---
+++ sw-staff.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = "cobras-staff-v1";
+const ASSETS = ["./", "./staff.html", "./staff.js", "./styles.css", "./cobras-logo.png", "./manifest-staff.json"];
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
+self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
